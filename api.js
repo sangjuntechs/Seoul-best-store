a21567b2fb20c71f33e78a494238f6c0cc43a414
sangjunpark <juntech0421@github.com>
2020-10-10 16:59:57 +0900

    styling

--- api.js
+++ api.js
@@ -17,9 +17,13 @@ function createTable(data) {
 
     for (let i =0; i < data.length; i++) {
         let api_data = `
+        <div class='store_wrap'>
             <h1>${data[i].SH_NAME}</h1>
-            <p>${data[i].SH_ADDR}</p>
-            <p>${data[i].SH_PRIDE}</p>
+            <p class='add'>${data[i].SH_ADDR}</p>
+            <p class='des'>${data[i].SH_PRIDE}</p>
+            <img src='${data[i].SH_PHOTO}'>
+            <img class='store_logo' src="https://upload.wikimedia.org/wikipedia/commons/thumb/f/f6/Logo_of_Seoul%2C_South_Korea.svg/1024px-Logo_of_Seoul%2C_South_Korea.svg.png"/>
+        <div>
         `
         table.innerHTML += api_data
     }
